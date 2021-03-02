a53a20a7333ccda26c29d86ce76aeef713a50947
nick <youjiancau@163.com>
2021-03-02 17:30:05 +0800

    fix bug

--- src/components/EbookUpload/index.vue
+++ src/components/EbookUpload/index.vue
@@ -1,7 +1,30 @@
 <template>
-  <div class="upload-container">
-    <el-upload :action="action"
-               :headers="headers">请上传
+  <div class="singleImageUpload2 upload-container">
+    <el-upload
+      :action="action"
+      :headers="headers"
+      :multiple="false"
+      :limit="1"
+      :before-upload="beforeUpload"
+      :on-success="onSuccess"
+      :on-error="onError"
+      :on-remove="onRemove"
+      :file-list="fileList"
+      :on-exceed="onExceed"
+      :disabled="disabled"
+      drag
+      show-file-list
+      class="image-uploader"
+      accept="application/epub+zip"
+    >
+      <i class="el-icon-upload" />
+      <div v-if="fileList.length === 0" class="el-upload__text">
+        请将电子书拖入或
+        <em>点击上传</em>
+      </div>
+      <div v-else class="el-upload__text">
+        图书已上传
+      </div>
     </el-upload>
   </div>
 </template>
@@ -10,6 +33,24 @@
 import { getToken } from '@/utils/auth'
 
 export default {
+  name: 'EbookUpload',
+  props: {
+    fileList: {
+      type: Array,
+      default() {
+        return []
+      }
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+  data() {
+    return {
+      action: `${process.env.VUE_APP_BASE_API}book/upload`
+    }
+  },
   computed: {
     headers() {
       return {
@@ -17,14 +58,60 @@ export default {
       }
     }
   },
-  data() {
-    return {
-      action: `${process.env.VUE_APP_BASE_API}/book/upload`
+  methods: {
+    onRemove() {
+      this.$message({
+        message: '电子书删除成功',
+        type: 'success'
+      })
+      this.$emit('onRemove')
+    },
+    onExceed() {
+      this.$message({
+        message: '每次只能上传一本电子书',
+        type: 'warning'
+      })
+    },
+    beforeUpload(file) {
+      this.$emit('beforeUpload', file)
+    },
+    onSuccess(response, file) {
+      console.log('onSuccess', response, file)
+      const { code, msg, data } = response
+      if (code !== 0) {
+        this.$message({
+          message: (msg && `上传失败，失败原因：${msg}`) || '上传失败',
+          type: 'error'
+        })
+        this.$emit('onError', data)
+      } else {
+        this.$message({
+          message: '上传成功',
+          type: 'success'
+        })
+        this.$emit('onSuccess', data)
+      }
+    },
+    onError(err) {
+      const errMsg = (err.message && JSON.parse(err.message)) || '上传失败'
+      this.$message({
+        message: (errMsg.msg && `上传失败，失败原因：${errMsg.msg}`) || '上传失败',
+        type: 'error'
+      })
+      this.$emit('onError', err)
     }
   }
 }
 </script>
 
-<style scoped>
+<style lang="scss" scoped>
+  .upload-container {
+    width: 100%;
+    height: 100%;
+    position: relative;
 
+    .image-uploader {
+      height: 100%;
+    }
+  }
 </style>
